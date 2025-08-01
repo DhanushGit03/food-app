075f6f37a0305e7eefa5764cf73aa5f1ede67418
Dhanush Gosala <dhanush908065@gmail.com>
2025-08-01 14:52:54 +0530

    About fetchData() in incurred in here

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,11 +5,16 @@ import { useState, useEffect } from "react";
 const Body = () => {
   const [listOfRestaurents, setlistOfRestaurents] = useState(resList);
   useEffect(() => {
-    console.log("UseEffect called");
+    fetchData();
   }, []);
 
-  //First Body is rendered and then the UseEffect will get rendered.
-  console.log("Body rendered");
+  //Fetch data concept
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.4650551&lng=73.8341982&collection=83647&tags=layout_CCS_Chinese&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
+    const json = await data.json();
+    console.log(json);
+    setlistOfRestaurents(json.data.cards[3].card.card.info);
+  };
 
   return (
     <div className="body">
